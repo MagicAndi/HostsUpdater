e1bd77dd2ff4d0cea80a353ee56ae7cc54b4a50c
Andy Parkhill <a.parkhill@qub.ac.uk>
2019-10-18 12:32:04 +0100

    Whitespace change.

--- HostsUpdater/ApplicationConfiguration.cs
+++ HostsUpdater/ApplicationConfiguration.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Westwind.Utilities.Configuration;
 
-
 namespace HostsUpdater
 {
     public class ApplicationConfiguration : AppConfiguration
